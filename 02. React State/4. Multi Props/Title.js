71a4f9f501705dcc5f566dd3f9ef84175f04d7ed
imronxz <imronxz@gmail.com>
2022-01-01 17:39:44 +0700

    02. Mutiple Props

---
+++ 02. React State/4. Multi Props/Title.js
@@ -0,0 +1,10 @@
+// TODO: Title({a, b}) destructuring from the object
+export default function Title({ title, subtitle }) {
+  return (
+    <div>
+      <h1 className="title">{title}</h1>
+      <br />
+      <h3 className="subtitle">{subtitle}</h3>
+    </div>
+  );
+}
