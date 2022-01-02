f8b0c4b71600017566406b456227947412b06cab
imronxz <imronxz@gmail.com>
2022-01-02 22:32:55 +0700

    06. New Project Magic Memory

---
+++ react-ninja_03_magic-memory/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Magic Match 🤼</h1>
+      <button>New Game 🎲</button>
+    </div>
+  );
+}
+
+export default App;
