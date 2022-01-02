d64a6be0982581e41d1b8f1b6ac7176a2a48ab6d
imronxz <imronxz@gmail.com>
2022-01-02 15:45:45 +0700

    04. Fetching API with axios and useEffect

---
+++ react-ninja_02_tripago/src/components/TripList.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+// Styles
+import './TripList.css';
+
+export default function TripList() {
+  const [trips, setTrips] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/trips').then((res) => {
+      setTrips(res.data);
+    });
+
+    /* fetch('http://localhost:3001/trips')
+      .then((res) => res.json())
+      .then((json) => setTrips(json)); */
+  }, []);
+
+  console.log(trips);
+
+  return (
+    <div className="trip-list">
+      <h2>Trip List</h2>
+      <ul>
+        {trips.map((trip) => (
+          <li key={trip.id}>
+            <h3>{trip.title}</h3>
+            <p>{trip.price}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
