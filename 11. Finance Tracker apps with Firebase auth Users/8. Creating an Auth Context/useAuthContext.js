7c62edaf40e99324da471bf1a7f5feebdf395efa
imronxz <imronxz@gmail.com>
2022-01-07 14:24:08 +0700

    11. Creating an Auth Context

---
+++ 11. Finance Tracker apps with Firebase auth Users/8. Creating an Auth Context/useAuthContext.js
@@ -0,0 +1,13 @@
+import { AuthContext } from './context/AuthContext';
+import { useContext } from 'react';
+
+export const useAuthCOntexst = () => {
+  const context = useContext(AuthContext);
+  // TODO: jika tidak ada context, maka error
+  if (!context) {
+    throw new Error(
+      'useAuthContext must be used within an AuthContextProvider',
+    );
+  }
+  return context;
+};
