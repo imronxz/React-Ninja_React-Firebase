a49e8a5c3a7e0568142940d6e33dfd388b96ddd6
imronxz <imronxz@gmail.com>
2022-01-07 22:04:09 +0700

    11 .Creating a Logout Hook

---
+++ 11. Finance Tracker apps with Firebase auth Users/10 .Creating a Logout Hook/hooks/useLogout.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import { projectAuth } from '../firebase/config';
+import { useAuthContext } from './useAuthContext';
+
+export const useLogout = () => {
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
+
+  const logout = async () => {
+    setError(null);
+    setIsPending(true);
+
+    // TODO: Logout user
+    try {
+      // * Signs out the current user with signOut firebase methdos.
+      await projectAuth.signOut();
+
+      // * Dispatch logout action
+      dispatch({ type: 'LOGOUT' });
+
+      setIsPending(false);
+      setError(null);
+    } catch (error) {
+      console.log(error.message);
+      setError(error.message);
+      setIsPending(false);
+    }
+  };
+
+  // TODO: Return logout function
+  return { error, isPending, logout };
+};
