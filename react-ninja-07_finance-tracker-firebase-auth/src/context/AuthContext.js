18682b6377bd87939b66e8d7cacef8eac6df80ab
imronxz <imronxz@gmail.com>
2022-01-09 03:05:45 +0700

    13. object color

--- react-ninja-07_finance-tracker-firebase-auth/src/context/AuthContext.js
+++ react-ninja-07_finance-tracker-firebase-auth/src/context/AuthContext.js
@@ -66,7 +66,7 @@ export const AuthContextProvider = ({ children }) => {
     });
   }, []);
 
-  console.log('Auth Context state:', state);
+  // console.log('Auth Context state:', state);
 
   return (
     <AuthContext.Provider
