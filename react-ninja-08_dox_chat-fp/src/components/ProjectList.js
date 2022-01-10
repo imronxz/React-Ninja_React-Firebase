395c466460fcb533de10090c01fe2374036fd70a
imronxz <imronxz@gmail.com>
2022-01-11 02:22:49 +0700

    14. Project Details Page

--- react-ninja-08_dox_chat-fp/src/components/ProjectList.js
+++ react-ninja-08_dox_chat-fp/src/components/ProjectList.js
@@ -12,7 +12,7 @@ export default function ProjectList({ projects }) {
     <div className="project-list">
       {projects.length === 0 && <p>Tidak ada project yang ditemukan</p>}
       {projects.map((project) => (
-        <Link to={`/project/${project.id}`} key={project.id}>
+        <Link to={`/projects/${project.id}`} key={project.id}>
           <h4>{project.name}</h4>
           <p>Dibuat Pada Tanggal: {project.date.toDate().toLocaleDateString()}</p>
           <div className="assigned-to">
