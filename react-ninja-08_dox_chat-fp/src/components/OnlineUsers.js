9f2cf81faecd8999c4d81123f9e6a3b6189e0b60
imronxz <imronxz@gmail.com>
2022-01-10 22:23:04 +0700

    14. Fetching Users

---
+++ react-ninja-08_dox_chat-fp/src/components/OnlineUsers.js
@@ -0,0 +1,26 @@
+// Costum Hooks
+import { useCollection } from '../hooks/useCollection';
+
+// components
+import Avatar from './Avatar';
+
+// Styles
+import './OnlineUsers.css';
+
+export default function OnlineUsers() {
+  const { documents, error } = useCollection('users');
+
+  return (
+    <div className="user-list">
+      <h2>Mahluk Online 🟢 </h2>
+      {error && <div className="error">{error}</div>}
+      {documents &&
+        documents.map((user) => (
+          <div key={user.id} className="user-list-item">
+            <span>{user.displayName}</span>
+            <Avatar src={user.photoURL} />
+          </div>
+        ))}
+    </div>
+  );
+}
